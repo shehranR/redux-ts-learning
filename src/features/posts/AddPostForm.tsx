fc907d506ffd967e5850fc7929c207bf5ec6f777
shehranR <shehranr157@gmail.com>
2026-01-15 18:45:19 +0600

    part3 of redux essentials complete

---
+++ src/features/posts/AddPostForm.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { nanoid } from '@reduxjs/toolkit'
+
+import { useAppDispatch } from '@/app/hooks'
+
+import {type Post, postAdded} from '@/features/posts/postsSlice'
+
+interface AddPostFormFields extends HTMLFormControlsCollection {
+    postTitle: HTMLInputElement,
+    postContent: HTMLTextAreaElement
+}
+
+interface AddPostFormElements extends HTMLFormElement {
+    readonly elements: AddPostFormFields
+}
+
+export const AddPostForm = () => {
+    const dispatch = useAppDispatch();
+
+    const handleSubmit = (e: React.FormEvent<AddPostFormElements>) => {
+        e.preventDefault()
+
+        const { elements } = e.currentTarget;
+        const payload: Post = {
+            id: nanoid(), 
+            title: elements.postTitle.value,
+            content: elements.postContent.value
+        }
+        dispatch(postAdded(payload));
+
+        e.currentTarget.reset();
+    }
+
+    return (
+        <section>
+            <h2>Add a New Post</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="postTitle">Post Title:</label>
+                <input type="text" id="postTitle" defaultValue="" required />
+                <label htmlFor="postContent">Content:</label>
+                <textarea
+                    id="postContent"
+                    name="postContent"
+                    defaultValue=""
+                    required
+                />
+                <button>Save Post</button>
+            </form>
+        </section>
+    )
+}
